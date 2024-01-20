8eac7f2a4c64e3cc7a7ca0cd407755679b175e95
maridius98 <maridius98@yahoo.com>
2024-01-20 19:33:35 +0100

    trade works?

--- Database-access/Controllers/TradeController.cs
+++ Database-access/Controllers/TradeController.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Neo4j.Driver;
+using ServiceStack.Redis;
+using Cache;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Databaseaccess.Models;
-using System.Data;
+using ServiceStack.Text;
 
 namespace Databaseaccess.Controllers
 {
@@ -13,14 +15,17 @@ namespace Databaseaccess.Controllers
     public class TradeController : ControllerBase
     {
         private readonly IDriver _driver;
+        private readonly RedisCache cache;
+        private readonly string singularKey = "trade";
+        private readonly string pluralKey = "trades";
 
-        public TradeController(IDriver driver)
+        public TradeController(IDriver driver, RedisCache redisCache)
         {
             _driver = driver;
+            cache = redisCache;
         }
 
-
-        [HttpPost("AddTrade")]
+        [HttpPost("AddTrade")] // radi kesiranje
         public async Task<IActionResult> AddTrade(TradeCreateDto trade)
         {
             try
@@ -29,7 +34,7 @@ namespace Databaseaccess.Controllers
                 {
                     var parameters = new
                     {
-                        isFinalized="false",  
+                        isFinalized=false,  
                         receiverGold=trade.ReceiverGold, 
                         requesterGold=trade.RequesterGold,
                         startedAt=DateTime.Now,
@@ -98,27 +103,57 @@ namespace Databaseaccess.Controllers
                         })
                             
                         WITH n, playerReceiver_, playerRequester_, receiverItemsList, requesterItemsList
-                            
-                        SET playerReceiver_.gold = playerReceiver_.gold + $receiverGold           
-                        SET playerRequester_.gold = playerRequester_.gold - $requesterGold          
+                             
+                        CREATE (n)-[:RECEIVER]->(playerReceiver_)
+                        CREATE (n)-[:REQUESTER]->(playerRequester_)
 
                         FOREACH (item IN receiverItemsList |
-                                CREATE (n)-[:RECEIVER_ITEM]->(item)
-                            )
+                            MERGE (n)-[:RECEIVER_ITEM]->(item)
+                        )
 
-                            FOREACH (item IN requesterItemsList |
-                                CREATE (n)-[:REQUESTER_ITEM]->(item)
-                            )
-                            
-                            CREATE (n)-[:RECEIVER]->(playerReceiver_)
-                            CREATE (n)-[:REQUESTER]->(playerRequester_)
-                            
-                        return n"
-                        ;
+                        FOREACH (item IN requesterItemsList |
+                            MERGE (n)-[:REQUESTER_ITEM]->(item)
+                        )
 
-                        var result=await session.RunAsync(query, parameters);
+                        return Id(n) as tradeID
+                        "
+                        ; 
 
-                        return Ok();
+                        var returnQuery = @"MATCH (receiver:Player)<-[relReceiver:RECEIVER]-(trade:Trade)-[relRequester:REQUESTER]->(requester:Player)
+                                        ,(recItem:Item)<-[relReceiverItem:RECEIVER_ITEM]-(trade)-[relRequesterItem:REQUESTER_ITEM]->(reqItem:Item)
+                                        WHERE Id(trade) = $ID
+                                            OPTIONAL MATCH (recItem)-[r:HAS]->(a:Attributes)
+                                            OPTIONAL MATCH (reqItem)-[r:HAS]->(a:Attributes)
+                                        RETURN 
+                                        trade as tradeInfo, 
+                                        requester, 
+                                        receiver, 
+                                        COLLECT({ item: recItem,
+                                                    attributes: CASE WHEN recItem:Gear THEN a ELSE NULL END
+                                                }) AS itemsRec,
+
+                                        COLLECT({ item: reqItem,
+                                                    attributes: CASE WHEN reqItem:Gear THEN a ELSE NULL END
+                                                }) AS itemsReq";
+
+                        var idCursor = await session.RunAsync(query, parameters);
+                        var idRecord = await idCursor.SingleAsync();
+                        var tradeID = idRecord ["tradeID"].As<int>();
+                        var cursor = await session.RunAsync(returnQuery, new {ID = tradeID});
+                        var record = await cursor.SingleAsync();   
+                        var tradeNode = record["tradeInfo"].As<INode>();
+                    
+                        var playerRec = record["receiver"].As<INode>();
+                        var playerReq = record["requester"].As<INode>();
+                        var recItem = record["itemsRec"].As<List<Dictionary<string, INode>>>();
+                        var reqItem = record["itemsReq"].As<List<Dictionary<string, INode>>>();
+                        Trade createTrade = new(tradeNode, playerRec, playerReq, recItem, reqItem);
+                        string key = singularKey + tradeID;
+                        await cache.SetDataAsync(key, createTrade, 1000);
+                        return Ok(createTrade);
+
+                        // SET playerReceiver_.gold = playerReceiver_.gold + $receiverGold           
+                        // SET playerRequester_.gold = playerRequester_.gold - $requesterGold     
                 }
             }
             catch (Exception ex)
@@ -127,7 +162,6 @@ namespace Databaseaccess.Controllers
             }
         }
 
-
         [HttpGet("GetAllTrades")]
         public async Task<IActionResult> GetAllTrades()
         {
@@ -135,8 +169,13 @@ namespace Databaseaccess.Controllers
             {
                 using (var session = _driver.AsyncSession())
                 {
-                    var result = await session.ExecuteReadAsync(async tx =>
+                    string key = "trades";
+                    var keyExists = await cache.CheckKeyAsync(key);
+                    if (keyExists)
                     {
+                        var t = await cache.GetDataAsync<List<Trade>>(key);
+                        return Ok(t);          
+                    }
                         var query = @"MATCH (receiver:Player)<-[relReceiver:RECEIVER]-(trade:Trade)-[relRequester:REQUESTER]->(requester:Player),
                                             (recItem:Item)<-[relReceiverItem:RECEIVER_ITEM]-(trade)-[relRequesterItem:REQUESTER_ITEM]->(reqItem:Item)
                                       OPTIONAL MATCH (recItem)-[r:HAS]->(a:Attributes)
@@ -152,7 +191,7 @@ namespace Databaseaccess.Controllers
                                       COLLECT({ item: reqItem,
                                                 attributes: CASE WHEN reqItem:Gear THEN a ELSE NULL END
                                              }) AS itemsReq";
-                        var cursor = await tx.RunAsync(query);
+                        var cursor = await session.RunAsync(query);
                         var resultList = new List<Trade>();
 
                         await cursor.ForEachAsync(record =>
@@ -166,10 +205,8 @@ namespace Databaseaccess.Controllers
                             resultList.Add(trade);
                         });
 
-                        return resultList;
-                    });
-
-                    return Ok(result);
+                        await cache.SetDataAsync(key, resultList, 1000);
+                        return Ok(resultList);
                 }
             }
             catch (Exception ex)
@@ -177,6 +214,7 @@ namespace Databaseaccess.Controllers
                 return BadRequest(ex.Message);
             }
         }
+
 
         [HttpGet("GetPlayerTrades")]
         public async Task<IActionResult> GetPlayerTrades(int playerID)
@@ -221,42 +259,8 @@ namespace Databaseaccess.Controllers
             }
         }
 
-        [HttpGet("GetTradesBetweenTwoPlayers")]
-        public async Task<IActionResult> GetTradesBetweenTwoPlayers(int player1ID, int player2ID)
-        {
-            try
-            {
-                using (var session = _driver.AsyncSession())
-                {
-                    var result = await session.ExecuteReadAsync(async tx =>
-                    {
-                        var query = "MATCH (n1:Player)<-[:RECEIVER]-(n:Trade)-[:REQUESTER]->(n2:Player) WHERE id(n1)=$playeri1 and id(n2)=$playeri2 RETURN n";
-                        
-                        var parameters = new { playeri1 = player1ID,
-                        playeri2=player2ID};
-                        var cursor = await tx.RunAsync(query,parameters);
-                        var nodes = new List<INode>();
-
-                        await cursor.ForEachAsync(record =>
-                        {
-                            var node = record["n"].As<INode>();
-                            nodes.Add(node);
-                        });
-
-                        return nodes;
-                    });
-
-                    return Ok(result);
-                }
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-
-        [HttpPut("UpdateTrade")]
-        public async Task<IActionResult> UpdateTrade(TradeUpdateDto trade)
+        [HttpPut("FinalizeTrade")]
+        public async Task<IActionResult> FinalizeTrade(TradeUpdateDto trade)
         {
             try
             {
@@ -269,16 +273,17 @@ namespace Databaseaccess.Controllers
                                 SET n.endedAt=$endedAt
                                 RETURN n";
 
-                    var parameters = new 
-                    { 
+                    var parameters = new
+                    {
                         tradeID = trade.TradeID,
                         receiverGold = trade.ReceiverGold,
-                        requesterGold= trade.RequesterGold,
-                        isFinalized="true",
-                        endedAt=DateTime.Now
-                         
+                        requesterGold = trade.RequesterGold,
+                        isFinalized = true, 
+                        endedAt = DateTime.Now
                     };
+                    await cache.DeleteAsync(singularKey + trade.TradeID);
                     await session.RunAsync(query, parameters);
+
                     return Ok();
                 }
             }
@@ -287,7 +292,7 @@ namespace Databaseaccess.Controllers
                 return BadRequest(ex.Message);
             }
         }
-      
+
       
         [HttpDelete("DeleteTrade")]
         public async Task<IActionResult> DeleteTrade(int tradeID)
