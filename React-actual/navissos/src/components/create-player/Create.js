b9e684ef86e0d7364d8af265da98172e6df8cf9b
Mirko <mirkoat21@gmail.com>
2024-01-25 23:27:43 +0100

    Ispravka

--- React-actual/navissos/src/components/create-player/Create.js
+++ React-actual/navissos/src/components/create-player/Create.js
@@ -53,57 +53,59 @@ function Create() {
     };
 
     return (
-        <div className='create-frame'>
-            <div className='all-class-data'> In-game classes: 
-                {classData.map(classData => (
-                        <p>{classData.id} - {classData.properties.name}</p>
-                    ))}
+        <div className="whole-wrap">    
+            <div className='create-frame'>
+                <div className='all-class-data'> In-game classes: 
+                    {classData.map(classData => (
+                            <p>{classData.id} - {classData.properties.name}</p>
+                        ))}
+                </div>
+                <h1 className='create-title'>The Elder Scrolls: Navissos</h1>
+                    <h2 className='create-subtitle'>Start your journey into a land of nosql databases</h2>
+                    <input
+                        className='create-input'
+                        type="text"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        className='create-input'
+                        type="text"
+                        placeholder="Username"
+                        value={name}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input
+                        className='create-input'
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <input
+                        className='create-input'
+                        type="text"
+                        placeholder="Created at"
+                        value={createdAt}
+                        onChange={(e) => setCreatedAt(e.target.value)}
+                    />
+                    <input
+                        className='create-input'
+                        type="text"
+                        placeholder="Bio"
+                        value={bio}
+                        onChange={(e) => setBio(e.target.value)}
+                    />
+                    <input
+                        className='create-input'
+                        type="text"
+                        placeholder="Class ID"
+                        value={classId}
+                        onChange={(e) => setClassId(e.target.value)}
+                    />
+                    <button className='create-btn' onClick={handleCreate}>Start adventure!</button>
             </div>
-            <h1 className='create-title'>The Elder Scrolls: Navissos</h1>
-                <h2 className='create-subtitle'>Start your journey into a land of nosql databases</h2>
-                <input
-                    className='create-input'
-                    type="text"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    className='create-input'
-                    type="text"
-                    placeholder="Username"
-                    value={name}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                    className='create-input'
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <input
-                    className='create-input'
-                    type="text"
-                    placeholder="Created at"
-                    value={createdAt}
-                    onChange={(e) => setCreatedAt(e.target.value)}
-                />
-                <input
-                    className='create-input'
-                    type="text"
-                    placeholder="Bio"
-                    value={bio}
-                    onChange={(e) => setBio(e.target.value)}
-                />
-                <input
-                    className='create-input'
-                    type="text"
-                    placeholder="Class ID"
-                    value={classId}
-                    onChange={(e) => setClassId(e.target.value)}
-                />
-                <button className='create-btn' onClick={handleCreate}>Start adventure!</button>
         </div>
     );
 }
--- React-actual/navissos/src/components/player/Player.js
+++ React-actual/navissos/src/components/player/Player.js
@@ -4,6 +4,14 @@ import './Player.css';
 const Player = () => {
     const [player, setPlayer] = useState([]);
     const [playerId, setPlayerId] = useState('');
+    const [itemData, setItemData] = useState([]);
+    const [itemName, setItemName] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:5236/api/Item/GetAllItems')
+            .then(response => response.json())
+            .then(data => setItemData(data));
+    }, []);
 
     useEffect(() => {
         fetch('http://localhost:5236/api/Player/AllPlayers') 
@@ -26,8 +34,56 @@ const Player = () => {
             });
     };
     
+    const handleNPCCall = (npcId) => {
+        return fetch(`http://localhost:5236/api/NPC/GetNPC?npcId=${npcId}`)
+            .then(response => response.json())
+            .then(data => {
+                // Handle the response data if needed
+                return data;
+            })
+            .catch(error => {
+                // Handle the error if needed
+                console.error(error);
+            });
+    };
+
+    const handleLevelUpPlayer = () => {
+        fetch(`http://localhost:5236/api/Player/LevelUp?playerId=${playerId}`, {
+            method: 'PUT'
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Handle the response data if needed
+                console.log(data);
+            })
+            .catch(error => {
+                // Handle the error if needed
+                console.error(error);
+            });
+    };
+
+    const handleItemPlayer = () => {
+        fetch(`http://localhost:5236/api/Player/AddItem?itemName=${itemName}&playerID=${playerId}`, {
+            method: 'PUT'
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Handle the response data if needed
+                console.log(data);
+            })
+            .catch(error => {
+                // Handle the error if needed
+                console.error(error);
+            });
+    };
+
     return (
         <div>
+            <div className='all-item-data'> Items around the world: 
+                {itemData.map(itemData => (
+                        <p>{itemData.item.id} - {itemData.item.properties.name}</p>
+                    ))}
+            </div>
             <h1 className='i-p'>Players</h1>
             <div className='players'>
                 {player.map(player => (
@@ -54,10 +110,26 @@ const Player = () => {
                     </button>
                 ))}
             </div>
-            <h1 className='i-p'>Admin-Delete a player</h1>
+            <h1 className='i-p'>Admin: Delete a player</h1>
             <div className='delete-player'>
-                <input type="number" placeholder="Enter player ID" value={playerId} onChange={e => setPlayerId(e.target.value)} />
+                <input type="number" placeholder="Enter player ID" onChange={e => setPlayerId(e.target.value)} />
                 <button onClick={handleDeletePlayer}>Delete Player</button>
+            </div>
+            <h1 className='i-p'>Admin: Level up a player</h1>
+            <div className='level-player'>
+                <input type="number" placeholder="Enter player ID" onChange={e => setPlayerId(e.target.value)} />
+                <button onClick={handleLevelUpPlayer}>Level up!</button>
+            </div>
+            <h1 className='i-p'>Admin: Give player an item</h1>
+            <div className='item-player'>
+                <input type="number" placeholder="Enter player ID" onChange={e => setPlayerId(e.target.value)} />
+                <input type="text" placeholder="Enter item name" onChange={e => setItemName(e.target.value)} />
+                <button onClick={handleItemPlayer}>Give</button>
+            </div>
+            <h1 className='i-p'>Gamewise: See a possible ally</h1>
+            <div className='npc-player'>
+                <input type="number" placeholder="Enter npc ID" onChange={e => setPlayerId(e.target.value)} />
+                <button onClick={handleNPCCall}>Call</button>
             </div>
         </div>
     );
